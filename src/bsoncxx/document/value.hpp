b0f7fde4f5970fb9813bb6ff8f00de7d32e58199
rayangler <rayangler@users.noreply.github.com>
2020-09-24 12:53:10 -0400

    Lint

--- src/bsoncxx/document/value.hpp
+++ src/bsoncxx/document/value.hpp
@@ -83,10 +83,10 @@ class BSONCXX_API value {
     value(value&&) noexcept = default;
     value& operator=(value&&) noexcept = default;
 
-//    template<typename T>
-//    explicit value(T& user_object) {
-//        to_bson(user_object, *this);
-//    }
+    //    template<typename T>
+    //    explicit value(T& user_object) {
+    //        to_bson(user_object, *this);
+    //    }
 
     ///
     /// Get a view over the document owned by this value.
@@ -101,7 +101,7 @@ class BSONCXX_API value {
     BSONCXX_INLINE operator document::view() const noexcept;
 
     // Function to help with serialization
-    template<typename T>
+    template <typename T>
     T get() {
         T temp_object{};
         from_bson(temp_object, this->view());
